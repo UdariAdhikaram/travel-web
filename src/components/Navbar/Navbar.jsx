c54c547ab4119275b7cf3ea9246e2ae6bf857fea
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-05 10:43:27 +0530

    update navbar box shadow

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
         <header className="header flex">
 
             <div className="logoDiv">
-                <a href=" " className="logo flex">
+                <a href="#" className="logo flex">
                     <h1><MdOutlineTravelExplore className="icon"/>  Trippy.</h1>
                 </a>
             </div>
@@ -20,36 +20,36 @@ export const Navbar = () => {
                 <ul className="navLists flex">
                     
                     <li className="navItem">
-                        <a href=" " className="navLink">Home</a>
+                        <a href="#" className="navLink">Home</a>
                     </li>
 
                     <li className="navItem">
-                        <a href=" " className="navLink">Packages</a>
+                        <a href="#" className="navLink">Packages</a>
                     </li>
 
                     <li className="navItem">
-                        <a href=" " className="navLink">Shop</a>
+                        <a href="#" className="navLink">Shop</a>
                     </li>
 
                     <li className="navItem">
-                        <a href=" " className="navLink">About</a>
+                        <a href="#" className="navLink">About</a>
                     </li>
 
                     <li className="navItem">
-                        <a href=" " className="navLink">Pages</a>
+                        <a href="#" className="navLink">Pages</a>
                     </li>
 
                     <li className="navItem">
-                        <a href=" " className="navLink">News</a>
+                        <a href="#" className="navLink">News</a>
                     </li>
 
                     <li className="navItem">
-                        <a href=" " className="navLink">Contact</a>
+                        <a href="#" className="navLink">Contact</a>
                     </li>
 
-                    <li className="navItem btn">
-                        <a href=" " className="navLink">BOOK NOW</a>
-                    </li>
+                    <button className='btn'>
+                        <a href='#'>BOOK NOW</a>
+                    </button>
                 </ul>
 
             <div className="closeNavbar">
