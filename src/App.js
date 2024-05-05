469183a74a9f2971f1c6c8294d40ac1a73c839d7
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-05 23:37:00 +0530

    create lable in home page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import './app.css'
 import Navbar from './components/Navbar/Navbar'
 //import Footer from './components/Footer/Footer'
-//import Home from './components/Home/Home'
+import Home from './components/Home/Home'
 //import Main from './components/Main/Main'
 
 const App = () => {
   return (
     <>
       <Navbar/>
-    {/*  <Home/>
+      <Home/>
+      {/*
       <Main/>
   <Footer/> */}
     </>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import './home.css'
+import video from '../../Assets/video2.mp4'
+import { GrLocation } from 'react-icons/gr'
 
 const Home = () => {
   return (
-    <div>home</div>
+    <section className='home'>
+      <div className="overlay"></div>
+      <video src={video} muted autoPlay loop type="video2/mp4"></video>
+    
+      <div className="homeContent container">
+        <div className='textDiv'>
+
+          <span className='smallText'>
+            Our Packages
+          </span>
+
+          <h1 className='homeTitle'>
+            Search your Holiday
+          </h1>
+
+        </div>
+
+        <div className="cardDiv grid">
+          <div className="destinationInput">
+            <label htmlFor="city">Search your destination:</label>
+            <div className="input flex">
+              <input type="text" placeholder='Enter name here....'/>
+              <GrLocation className='icon'/>
+            </div>
+            </div>
+
+            <div className="dateInput">
+            <label htmlFor="date">Select your date:</label>
+            <div className="input flex">
+              <input type="date"/>
+              </div>
+            </div>
+
+            <div className="priceInput">
+              <div className='lable_total flex'>
+                <label htmlFor='price'>Max price:</label>
+                <h3 className='total'>LKR 10,000</h3>
+              </div>
+                <div className='input flex'>
+                  <input type="range" max="10000"
+                  min="5000"/>
+                </div>
+            </div>
+          </div>
+      </div>
+
+    
+    </section>
   )
 }
 
