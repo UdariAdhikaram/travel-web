4ab6d9dc9116f14ecfc2b893b9abb78287d644f0
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-09 22:24:28 +0530

    style footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
+import video2 from '../../Assets/video (2).mp4'
+import { FiSend } from "react-icons/fi"
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <section className="footer">
+      <div className="video">
+        <video src={video2} lool autoPlay muted type="video/mp4"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN Touch</small>
+            <h2>Travel With Us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input type="text" placeholder='Enter Email Address'/>
+            <button className="btn flex" type='submit'>
+                SEND <FiSend className="icon"/>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
