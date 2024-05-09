e72c2613429908c5ed2f0df210a07ef52192380b
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-05-09 21:18:46 +0530

    import video to footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './app.css'
 import Navbar from './components/Navbar/Navbar'
-//import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 import Main from './components/Main/Main'
 
